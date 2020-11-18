1b1a8300ea6eb06c7db43b11758aef27dad9e8c6
jossraquel18245 <71808090+jossraquel18245@users.noreply.github.com>
2020-11-17 16:47:43 -0800

    added eyes

--- sketch.js
+++ sketch.js
@@ -3,11 +3,22 @@ let poseNet;
 let pose;
 let skeleton;
 
+function preload(){
+  img = loadimage('assets/1.jpg');
+}
+
+
 function setup() {
   createCanvas(640, 480);
+
+//webcam
   video = createCapture(VIDEO);
+  //hide the webcam under canvas.
   video.hide();
+
+//create posenet
   poseNet = ml5.poseNet(video, modelLoaded);
+//turns this on
   poseNet.on('pose', gotPoses);
 }
 
@@ -24,19 +35,34 @@ function modelLoaded() {
 }
 
 function draw() {
+
+
+
+
+//draw it as image on canvas
   image(video, 0, 0);
 
   if (pose) {
 
     let eyeR = pose.rightEye;
     let eyeL = pose.leftEye;
-    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
 
+    //measure distance from camera by measuring distance between eyes
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+    image(img, pose.rightEye.x, pose .eye.y, d);
+    image(img, pose.leftEye.x, pose .eye.y, d);
+
+    //draw nose
     fill(245, 198, 218);
     rect(pose.nose.x, pose.nose.y, 64);
     fill(0, 0, 255);
+
+
+    //draw ellipses on the wrists
     ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
     ellipse(pose.leftWrist.x, pose.rightWrist.y, 32);
+
+
 
     for (let i = 0; i < pose.keypoints.length; i++){
       let x = pose.keypoints[i].position.x;
@@ -44,7 +70,7 @@ function draw() {
       fill(0,255,0);
       ellipse(x,y,16,16);
     }
-
+    //draw lines between points
     for (let i = 0; i < skeleton.length; i++){
       let a = skeleton[i][0];
       let b = skeleton[i][1];
